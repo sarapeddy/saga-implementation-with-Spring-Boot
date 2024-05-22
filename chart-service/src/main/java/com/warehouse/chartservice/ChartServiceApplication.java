34f514f7b07b0ae2cd1053df1c39d344c3dd3778
sarapeddy <269362@studenti.unimore.it>
2024-05-22 11:11:29 +0200

    Initial commit

---
+++ chart-service/src/main/java/com/warehouse/chartservice/ChartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.warehouse.chartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartServiceApplication.class, args);
+	}
+
+}
