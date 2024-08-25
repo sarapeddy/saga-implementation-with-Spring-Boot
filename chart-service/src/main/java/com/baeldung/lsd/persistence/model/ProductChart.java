f9749546bcfd7500e79e34dc8033f0c3fedd16aa
sarapeddy <269362@studenti.unimore.it>
2024-08-25 12:37:16 +0200

    Change the name of tables

--- chart-service/src/main/java/com/baeldung/lsd/persistence/model/ProductChart.java
+++ chart-service/src/main/java/com/baeldung/lsd/persistence/model/ProductChart.java
@@ -2,11 +2,7 @@ package com.baeldung.lsd.persistence.model;
 
 import java.util.Objects;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class ProductChart {
--- warehouse-service/src/main/resources/data.sql
+++ warehouse-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 
-INSERT INTO ProductWarehouse(id, code, name, description) VALUES (default, 'P1', 'Project 1', 'Description of Project 1');
-INSERT INTO ProductWarehouse(id, code, name, description) VALUES (default, 'P2', 'Project 2', 'About Project 2');
-INSERT INTO ProductWarehouse(id, code, name, description) VALUES (default, 'P3', 'Project 3', 'About Project 3');
+INSERT INTO Product_Warehouse(id, code, name, description) VALUES (default, 'P1', 'Project 1', 'Description of Project 1');
+INSERT INTO Product_Warehouse(id, code, name, description) VALUES (default, 'P2', 'Project 2', 'About Project 2');
+INSERT INTO Product_Warehouse(id, code, name, description) VALUES (default, 'P3', 'Project 3', 'About Project 3');
